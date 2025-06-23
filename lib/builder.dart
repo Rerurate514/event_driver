394633dd2eaae434850dd4051eb0156cfbadb935
Rerurate514 <rerurateyuto@gmail.com>
2025-06-23 10:18:16 +0900

    正しく生成ファイルが生成されるように修正

--- lib/builder.dart
+++ lib/builder.dart
@@ -4,4 +4,7 @@ import 'package:source_gen/source_gen.dart';
 import 'event_annotation_generator.dart';
 
 Builder eventBuilder(BuilderOptions options) =>
-    SharedPartBuilder([EventAnnotationGenerator()], 'event');
+  LibraryBuilder(
+    EventAnnotationGenerator(), 
+    generatedExtension: '.event.dart'
+  );
