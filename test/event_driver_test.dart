36b871ff9491aa3950919ec3f7bfd8c714747c84
Rerurate514 <rerurateyuto@gmail.com>
2025-06-23 17:58:01 +0900

    イベントハンドラの登録を修正し、不要なテストファイルを削除

--- test/event_driver_test.dart
+++ test/event_driver_test.dart
@@ -1,11 +1,24 @@
 import 'package:event_driver/event_driver.dart';
-import 'my_service.dart';
+import 'my_service.dart' as ms;
+import 'order_service.dart' as os;
 
 void main() {
   // すべてのイベントハンドラを登録
-  EventRegistry.registerAll();
+  ms.EventRegistry.registerAll();
+  os.EventRegistry.registerAll();
   
   // イベントを発火
   EventDriver().call("buttonPushed", ["login_button"]);
   EventDriver().call("formSubmitted", [{"username": "user1", "password": "****"}]);
+
+  EventDriver().call("orderPlaced", [{"orderId": "12345", "items": ["item1", "item2"]}]);
+  EventDriver().call("orderCanceled", ["12345", "Customer changed mind"]);
+  EventDriver().call("orderShipped", ["12345", "TRACK123456"]);
+  EventDriver().call("orderDelivered", ["12345"]);
+  EventDriver().call("orderReturned", ["12345", "Item was defective"]);
+  EventDriver().call("paymentReceived", ["12345", 100.0]);
+  EventDriver().call("paymentRefunded", ["12345", 100.0]);
+  EventDriver().call("itemOutOfStock", ["item1"]);
+  EventDriver().call("customerContacted", ["12345", "Customer inquired about order status"]);
+  EventDriver().call("orderNoteAdded", ["12345", "Please deliver before 5 PM"]);
 }
